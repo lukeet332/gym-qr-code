b71c744999ed9cdffa0f7b225fd379d929ef5f3c
lukeet <lukeetwebdev@gmail.com>
2023-09-02 19:28:04 +0200

    added base name

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "{gym-qr-code}",
+  base: "/gym-qr-code/",
   define: {
     "process.env": {},
   },
